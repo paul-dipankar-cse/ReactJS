bfef3d440c6e586e83330a6136861480ecfc5776
Dipankar Paul <dipankarpaul7021@gmail.com>
2023-12-28 14:34:19 +0530

    beginning of react

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Hello World | Dipankar Paul</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  
+
+  return (
+    <h1>Hello World With Vite | Dipankar Paul</h1>
+  )
+}
+
+export default App
