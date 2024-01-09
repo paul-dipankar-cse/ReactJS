8e691c449b724997cb0656766180949fe0c0dc90
Dipankar Paul <dipankarpaul7021@gmail.com>
2024-01-09 16:48:51 +0530

    cutom react

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,12 @@
 import Dp from "./dp"
 
 function App() {
-  
+
+  const username = "dipankar"
 
   return (
     <>
-    <h1>Hello World With Vite | Dipankar Paul</h1>
+    <h1>Hello World With Vite | {username}</h1>
     <Dp></Dp>
     </>
   )
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,9 +2,35 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+  return(
+    <div>
+      <h1>Custom App 1</h1>
+    </div>
+  )
+}
+
+// const ReactElement = {
+//   type: 'a',
+//   props: {
+//       href: 'https.//google.com',       
+//       target: '_blank'
+//   },
+//   children: 'click me to visit google'
+// }
+
+const AnotherElement = (
+  <a href="https://google.com">Visit Google</a>
+)
+
+const reactElement = React.createElement(
+  'a',
+  {href: "https://google.com", target: "_blank"},
+  'click Me to visit Google.com'
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+    <App/>
+  
 )
